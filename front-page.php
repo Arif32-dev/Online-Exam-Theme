9bfd7175d24fbba96de6723875e16792cf63f200
Arif32-dev <dragonwarrior062@gmail.com>
2020-07-14 21:18:36 +0600

    home page design finished

--- front-page.php
+++ front-page.php
@@ -96,31 +96,39 @@
     <h1>Notice Board</h1>
     <div class="notice_container">
         <div href="http://www.facebook.com" class="upcoming">
-                <h2>Upcoming Exam's</h2>
-               <a href="http://www.facebook.com">
-                    <h3>Department : Biology</h3>
-                    <h3>Exam Name : Biology Exam</h3>
-                    <span><strong>Date :</strong>10/07/2020</span>
-               </a>
+            <h2>Upcoming Exam's</h2>
+            <a href="http://www.facebook.com">
+                <h3>Department : Biology</h3>
+                <h3>Exam Name : Biology Exam</h3>
+                <span><strong>Date :</strong>10/07/2020</span>
+            </a>
 
         </div>
         <div class="previous">
-                 <h2>Previous Exam's</h2>
-               <a>
-                    <h3>Department : Mathmatics</h3>
-                    <h3>Exam Name : Math Exam</h3>
-                    <span><strong>Date :</strong>10/07/2020</span>
-               </a>
-               <a>
-                    <h3>Department : Mathmatics</h3>
-                    <h3>Exam Name : Math Exam</h3>
-                    <span><strong>Date :</strong>10/07/2020</span>
-               </a>
-               <a>
-                    <h3>Department : Mathmatics</h3>
-                    <h3>Exam Name : Math Exam</h3>
-                    <span><strong>Date :</strong>10/07/2020</span>
-               </a>
+            <h2>Previous Exam's</h2>
+            <a>
+                <h3>Department : Mathmatics</h3>
+                <h3>Exam Name : Math Exam</h3>
+                <span><strong>Date :</strong>10/07/2020</span>
+            </a>
+            <a>
+                <h3>Department : Mathmatics</h3>
+                <h3>Exam Name : Math Exam</h3>
+                <span><strong>Date :</strong>10/07/2020</span>
+            </a>
+            <a>
+                <h3>Department : Mathmatics</h3>
+                <h3>Exam Name : Math Exam</h3>
+                <span><strong>Date :</strong>10/07/2020</span>
+            </a>
+        </div>
+    </div>
+</section>
+<section class="oe_google_map">
+    <h1>Our Location</h1>
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe  id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
     </div>
 </section>
