6e2ecc96b302834f70e9ac6c71b5f668723509c7
Arif32-dev <dragonwarrior062@gmail.com>
2020-08-18 13:00:43 +0600

    404 page added

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header('header.php')?>
+<section class="oe-verifcation">
+        <div class="veri_container">
+            <div class="ver_msg">
+                <p>Sorry! Content not found</p>
+            </div>
+            <a href="<?php echo site_url('/') ?>">Back to Home</a>
+        </div>
+</section>
+<?php get_footer('footer.php')?>
